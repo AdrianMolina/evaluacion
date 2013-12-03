122fe2f9d193e731f0c55e0b7edabdd65065dd28
AdrianMolina <adrian6_6_6@hotmail.com>
2013-12-03 08:02:42 -0400

    refactorizando

--- src/test/java/evaluacion/bisiestoTest.java
+++ src/test/java/evaluacion/bisiestoTest.java
@@ -2,19 +2,25 @@ package evaluacion;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class bisiestoTest {
+	
+	private Bisiesto b;
+	
+	@Before
+	public void setUp(){
+		b = new Bisiesto();
+	}
 
 	@Test
 	public void retornarAnio() {
-		Bisiesto b = new Bisiesto();
 		assertEquals("1995", b.esBisiesto(1995));
 	}
 	
 	@Test
 	public void anioDivisibleEntreCuatro() {
-		Bisiesto b = new Bisiesto();
 		assertEquals("es divisible", b.esBisiesto(1996));
 	}
 
